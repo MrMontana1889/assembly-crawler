50c724dd3a9974cf7988e3abf4674e2517a9fe66
Kris Culin <Kris.Culin@bentley.com>
2021-09-28 19:43:04 -0400

    Getting head around reflection API in .NET

--- Development/assembly-crawler/AssemblyCrawler/AssemblyCrawler.cs
+++ Development/assembly-crawler/AssemblyCrawler/AssemblyCrawler.cs
@@ -24,10 +24,34 @@ namespace AssemblyCrawler
 			foreach (Type type in types)
 			{
 				// Ignore abstract classes
-				if (type.IsAbstract)
+				if (type.IsAbstract && !type.IsInterface)
 					continue;
 
-				Trace.WriteLine(type.FullName);
+				if (type.IsInterface)
+				{
+					Console.Write($"{type.FullName}");
+					if (type.IsGenericType)
+						Console.WriteLine(" (Generic)");
+					else
+						Console.WriteLine("");
+
+					var methods = type.GetMethods();
+					foreach (var method in methods)
+					{
+						if (method.Name.StartsWith("get_") ||
+							method.Name.StartsWith("set_"))
+							continue;
+						Console.WriteLine($"\t{method.Name}");
+					}
+
+					var properties = type.GetProperties();
+					foreach (var property in properties)
+						Console.WriteLine($"\t{property.Name}");
+
+					var interfaces = type.GetInterfaces();
+					foreach (var interf in interfaces)
+						Console.WriteLine($"\t\t{interf.Name}");
+				}
 			}
 		}
 		#endregion
