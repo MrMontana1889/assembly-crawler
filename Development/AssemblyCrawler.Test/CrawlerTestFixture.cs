942329306f0f4b01dda9af5156e962b62ac09607
Akshaya Niraula <xniraula@live.com>
2021-09-29 10:44:15 -0600

    Added puthon stub writer for class and properties

--- Development/AssemblyCrawler.Test/CrawlerTestFixture.cs
+++ Development/AssemblyCrawler.Test/CrawlerTestFixture.cs
@@ -1,10 +1,16 @@
 ﻿// CrawlerTestFixture.cs
 // Copyright (c) 2021 Kristopher L. Culin See LICENSE for details
 
+using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Reflection;
+using System.Text;
 using AssemblyCrawler.Generators;
+using AssemblyCrawler.Library;
 using NUnit.Framework;
 using TestAssemblyNET48.Water;
+using TestAssemblyNET48.Water.Domain.ModelingObjects.NetworkElements;
 
 namespace AssemblyCrawler.Test
 {
@@ -30,6 +36,56 @@ namespace AssemblyCrawler.Test
 
 			IAssemblyCrawler crawler = new AssemblyCrawler();
 			crawler.Crawl(testAssembly, generator);
+		}
+
+
+		[Test]
+		public void TestWriteStub()
+        {
+			var filePath = @"D:\Temp\pythonStybText.pyi";
+			if (File.Exists(filePath))
+				File.Delete(filePath);
+
+            using (var fileStream = new FileStream(filePath, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite))
+            {
+				using(var streamWriter = new StreamWriter(fileStream, Encoding.ASCII))
+                {
+					Assembly testAssembly = Assembly.GetAssembly(typeof(OpenFlowsWater));
+					var testType = typeof(IPipeInput);
+					testType = typeof(IBaseLinkInput);
+
+					var inheretedTypes = new List<Type>();
+
+                    foreach (var interf in testType.GetInterfaces())
+                    {
+						if (!interf.IsGenericType)
+							inheretedTypes.Add(interf);
+					}
+
+
+					// Write class
+					PythonStubWriterLibrary.WritePythonClassDefinition(streamWriter, testType.Name, inheretedTypes, PythonStubWriterLibrary.BlankDocString);
+
+					// Get methods
+					var methods = testType.GetMethods();
+
+
+                    // Write properties
+                    foreach (var method in methods)
+                    {
+						if (method.Name.StartsWith("get_"))
+							PythonStubWriterLibrary.WritePythonProperty(streamWriter, method.Name.Substring(4), method.ReturnType, PythonStubWriterLibrary.BlankDocString);
+
+
+						if (method.Name.StartsWith("set_"))
+							PythonStubWriterLibrary.WritePythongPropertySetter(streamWriter, method.Name.Substring(4), method.GetParameters()[0].ParameterType);
+					}
+
+
+
+					streamWriter.Flush();
+				}
+            }
 		}
 		#endregion
 	}
---
+++ Development/assembly-crawler/AssemblyCrawler/Library/TypeLibrary.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssemblyCrawler.Library
+{
+	public static class TypeLibrary
+	{
+		public static string ConvertTypeToPythonType(Type type)
+		{
+			if (type == null) return "None";
+
+			switch (Type.GetTypeCode(type))
+			{
+				case TypeCode.Byte:
+				case TypeCode.SByte:
+				case TypeCode.UInt16:
+				case TypeCode.UInt32:
+				case TypeCode.UInt64:
+				case TypeCode.Int16:
+				case TypeCode.Int32:
+				case TypeCode.Int64:
+					return "int";
+
+				case TypeCode.Single:
+				case TypeCode.Decimal:
+				case TypeCode.Double:
+					return "float";
+
+				case TypeCode.Boolean:
+					return "bool";
+
+				case TypeCode.Char:
+				case TypeCode.String:
+					return "str";
+
+				case TypeCode.DateTime:
+					return "datetime"; // must have "from datetime import datetime"
+
+				//case TypeCode.Object:
+				//	return "Any"; // must have "from typing import Any"
+
+				default:
+					return type.Name;
+
+			}
+		}
+	}
+}
