303a98102b5b7663602d1c4d1b14d2707b4bff5a
Kris Culin <kris@culin.org>
2021-09-29 04:01:13 -0400

    Testing out some API calls to figure out how best to crawl an assembly

--- Development/assembly-crawler/AssemblyCrawler/AssemblyCrawler.cs
+++ Development/assembly-crawler/AssemblyCrawler/AssemblyCrawler.cs
@@ -1,9 +1,11 @@
 ﻿// Crawler.cs
 // Copyright (c) 2021 Kristopher L. Culin.  See LICENSE for details.
 
+using System.Linq;
 using System;
 using System.Diagnostics;
 using System.Reflection;
+using System.Text;
 
 namespace AssemblyCrawler
 {
@@ -29,11 +31,10 @@ namespace AssemblyCrawler
 
 				if (type.IsInterface)
 				{
-					Console.Write($"{type.FullName}");
-					if (type.IsGenericType)
-						Console.WriteLine(" (Generic)");
-					else
-						Console.WriteLine("");
+					Console.WriteLine($"{RemoveNamespace(type)}:");
+					var interfaces = type.GetInterfaces();
+					foreach (var interf in interfaces)
+						Console.WriteLine($"\t\t{interf.Name}");
 
 					var methods = type.GetMethods();
 					foreach (var method in methods)
@@ -41,18 +42,98 @@ namespace AssemblyCrawler
 						if (method.Name.StartsWith("get_") ||
 							method.Name.StartsWith("set_"))
 							continue;
-						Console.WriteLine($"\t{method.Name}");
+						if (method.IsConstructor) continue;
+
+						Console.WriteLine($"\t{RemoveNamespace(method)}");
 					}
 
 					var properties = type.GetProperties();
 					foreach (var property in properties)
-						Console.WriteLine($"\t{property.Name}");
-
-					var interfaces = type.GetInterfaces();
-					foreach (var interf in interfaces)
-						Console.WriteLine($"\t\t{interf.Name}");
+					{
+						Console.WriteLine($"\t{RemoveNamespace(property)}");
+					}
 				}
 			}
+		}
+
+		private object RemoveNamespace(PropertyInfo property)
+		{
+			string? retVal = property.ToString();
+			if (retVal == null)
+				return "<unknown>";
+
+			string? ns = property.PropertyType.Namespace;
+			if (ns != null)
+			{
+				int index = retVal.IndexOf(ns);
+				if (index > -1)
+					retVal = retVal.Remove(index, ns.Length + 1);
+			}
+			return retVal;
+		}
+
+		private object RemoveNamespace(Type type)
+		{
+			string? retVal = type.ToString();
+			if (retVal == null)
+				return "<unknown>";
+
+			string? ns = type.Namespace;
+			if (ns != null)
+			{
+				int index = retVal.IndexOf(ns);
+				if (index > -1)
+					retVal = retVal.Remove(index, ns.Length + 1);
+			}
+			return retVal;
+		}
+
+		private string RemoveNamespace(MethodInfo method)
+		{
+			string? retVal = method.ToString();
+
+			if (retVal == null)
+				return "<unknown>";
+
+			string? ns = method.ReturnType.Namespace;
+			if (ns != null)
+			{
+				int index = retVal.IndexOf(ns);
+				if (index > -1)
+					retVal = retVal.Remove(index, ns.Length + 1);
+			}
+
+			var parameters = method.GetParameters();
+			foreach (var parameter in parameters)
+			{
+				ns = parameter.ParameterType.Namespace;
+				if (ns != null)
+				{
+					int index = retVal.IndexOf(ns);
+					if (index > -1)
+						retVal = retVal.Remove(index, ns.Length + 1);
+				}
+			}
+
+			var genericArguments = method.ReturnType.GetGenericArguments();
+			foreach (var arg in genericArguments)
+			{
+				if (arg != null)
+				{
+					ns = arg.Namespace;
+					if (ns != null)
+					{
+						int index = retVal.IndexOf(ns);
+						if (index > -1)
+							retVal = retVal.Remove(index, ns.Length + 1);
+					}
+				}
+			}
+
+			retVal = retVal.Replace("Int32", "int");
+
+			return retVal;
+
 		}
 		#endregion
 	}
