cb5016b74ad86437018fa20ab6a92dc47a10b968
Akshaya Niraula <xniraula@live.com>
2021-10-01 13:59:46 -0600

    on PyStub +OperatorOverload, ++static members

--- Development/AssemblyCrawler.Test/CrawlerTestFixture.cs
+++ Development/AssemblyCrawler.Test/CrawlerTestFixture.cs
@@ -14,60 +14,61 @@ using TestAssemblyNET48.Water.Domain.ModelingObjects.NetworkElements;
 
 namespace AssemblyCrawler.Test
 {
-	[TestFixture]
-	public class CrawlerTestFixture
-	{
-		#region Constructor
-		public CrawlerTestFixture()
-		{
+    [TestFixture]
+    public class CrawlerTestFixture
+    {
+        #region Constructor
+        public CrawlerTestFixture()
+        {
 
-		}
-		#endregion
+        }
+        #endregion
 
-		#region Tests
-		[Test]
-		public void TestSimpleCrawl()
-		{
-			Assembly testAssembly = Assembly.GetAssembly(typeof(OpenFlowsWater));
-			Assert.IsNotNull(testAssembly);
+        #region Tests
+        [Test]
+        public void TestSimpleCrawl()
+        {
+            Assembly testAssembly = Assembly.GetAssembly(typeof(OpenFlowsWater));
+            Assert.IsNotNull(testAssembly);
 
-			IAssemblyCrawler crawler = new AssemblyCrawler();
-			crawler.Crawl(testAssembly.Location);
-		}
-
-
-		[Test]
-		public void TestWriteStub()
-		{
-
-			Assembly testAssembly = Assembly.GetAssembly(typeof(OpenFlowsWater));
-			var testType = typeof(IPipeInput);
-			testType = typeof(IBaseLinkInput);
-			testType = typeof(IPipeResults);
-			testType = typeof(INetwork);
-			testType = typeof(Pipes);
-
-			var tempDir = Path.Combine(Path.GetTempPath(), "pythonStubs");
-			if (!Directory.Exists(tempDir))
-				Directory.CreateDirectory(tempDir);
-
-			var filePath = Path.Combine(Path.GetTempPath(),"pythonStubs", $"{testType.Name}.pyi");
-			if (File.Exists(filePath))
-				File.Delete(filePath);			
+            IAssemblyCrawler crawler = new AssemblyCrawler();
+            crawler.Crawl(testAssembly.Location);
+        }
 
 
-			using (var fileStream = new FileStream(filePath, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite))
-			{
-				using (var streamWriter = new StreamWriter(fileStream, Encoding.ASCII))
-				{
-					IStubGenerator generator = GeneratorLibrary.NewPythonStubGenerator(streamWriter);
-					Assert.IsNotNull(generator);
+        [Test]
+        public void TestWriteStub()
+        {
 
-					generator.GenerateTypeStub(testType);
-					streamWriter.Flush();
-				}
-			}
-		}
-		#endregion
-	}
+            Assembly testAssembly = Assembly.GetAssembly(typeof(OpenFlowsWater));
+            var testType = typeof(IPipeInput);
+            testType = typeof(IBaseLinkInput);
+            testType = typeof(IPipeResults);
+            testType = typeof(INetwork);
+            testType = typeof(Pipes);
+            testType = typeof(System.Drawing.Size);
+
+            var tempDir = Path.Combine(Path.GetTempPath(), "pythonStubs");
+            if (!Directory.Exists(tempDir))
+                Directory.CreateDirectory(tempDir);
+
+            var filePath = Path.Combine(Path.GetTempPath(), "pythonStubs", $"{testType.Name}.pyi");
+            if (File.Exists(filePath))
+                File.Delete(filePath);
+
+
+            using (var fileStream = new FileStream(filePath, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite))
+            {
+                using (var streamWriter = new StreamWriter(fileStream, Encoding.ASCII))
+                {
+                    IStubGenerator generator = GeneratorLibrary.NewPythonStubGenerator(streamWriter);
+                    Assert.IsNotNull(generator);
+
+                    generator.GenerateTypeStub(testType);
+                    streamWriter.Flush();
+                }
+            }
+        }
+        #endregion
+    }
 }
--- Development/assembly-crawler/AssemblyCrawler/Library/TypeConvertLibrary.cs
+++ Development/assembly-crawler/AssemblyCrawler/Library/TypeConvertLibrary.cs
@@ -7,76 +7,92 @@ using System.Linq;
 
 namespace AssemblyCrawler.Library
 {
-	public static class TypeConvertLibrary
-	{
+    public static class TypeConvertLibrary
+    {
 
-		#region Public Static Methods
+        #region Public Static Methods
 
-		public static string ToPythonType(Type type)
-		{
-			if (type == typeof(void)) return "None";
-			if (type == typeof(Enum))  return "Enum";
+        public static string ToPythonType(Type type)
+        {
+            if (type == typeof(void)) return "None";
+            if (type == typeof(Enum)) return "Enum";
 
-			if(type.IsGenericType 
-				&& (type.GetGenericTypeDefinition() == typeof(List<>) 
-				|| type.GetGenericTypeDefinition() == typeof(IList<>)))
-			{
-				var itemType = type.GetGenericArguments().Single();
-				return $"List[{ToPythonPrimitiveType(itemType)}]";
-			}
+            if (type.IsGenericType)
+            {
+                if (type.GetGenericTypeDefinition() == typeof(List<>)
+                || type.GetGenericTypeDefinition() == typeof(IList<>))
+                {
+                    var itemType = type.GetGenericArguments().Single();
+                    return $"List[{ToPythonPrimitiveType(itemType)}]";
+                }
 
-			if (type.IsGenericType 
-				&& (type.GetGenericTypeDefinition() == typeof(Dictionary<,>)
-				|| type.GetGenericTypeDefinition() == typeof(IDictionary<,>)))
-			{
-				var keyType = type.GetGenericArguments()[0];
-				var valueType = type.GetGenericArguments()[0];
-				return $"Dict[{ToPythonPrimitiveType(keyType)},{ToPythonPrimitiveType(valueType)}]";
-			}
+                if (type.GetGenericTypeDefinition() == typeof(Dictionary<,>)
+                || type.GetGenericTypeDefinition() == typeof(IDictionary<,>))
+                {
+                    var keyType = type.GetGenericArguments()[0];
+                    var valueType = type.GetGenericArguments()[0];
+                    return $"Dict[{ToPythonPrimitiveType(keyType)},{ToPythonPrimitiveType(valueType)}]";
+                }
 
-			return ToPythonPrimitiveType(type);
-		}
-		#endregion
+                if (type.GetGenericTypeDefinition() == typeof(IEnumerable<>))
+                {
+                    var itemType = type.GetGenericArguments().Single();
+                    return $"Iterator[{ToPythonPrimitiveType(itemType)}]";
+                }
 
-		#region Private Static Methdos
+                if (type.GetGenericTypeDefinition() == typeof(IComparer<>))
+                {
+                    var itemType = type.GetGenericArguments().Single();
+                    return $"Comparer[{ToPythonPrimitiveType(itemType)}]"; // Is Comparer a thing in python?
+                }
 
-		private static string ToPythonPrimitiveType(Type type)
-		{
-			switch (Type.GetTypeCode(type))
-			{
-				case TypeCode.Byte:
-				case TypeCode.SByte:
-				case TypeCode.UInt16:
-				case TypeCode.UInt32:
-				case TypeCode.UInt64:
-				case TypeCode.Int16:
-				case TypeCode.Int32:
-				case TypeCode.Int64:
-					return "int";
+                // TODO: Predicate, Action, Converter, ReadOnlyCollection, Comparison
+            }
 
-				case TypeCode.Single:
-				case TypeCode.Decimal:
-				case TypeCode.Double:
-					return "float";
 
-				case TypeCode.Boolean:
-					return "bool";
+            return ToPythonPrimitiveType(type);
+        }
+        #endregion
 
-				case TypeCode.Char:
-				case TypeCode.String:
-					return "str";
+        #region Private Static Methdos
 
-				case TypeCode.DateTime:
-					return "datetime"; // must have "from datetime import datetime"
+        private static string ToPythonPrimitiveType(Type type)
+        {
+            switch (Type.GetTypeCode(type))
+            {
+                case TypeCode.Byte:
+                case TypeCode.SByte:
+                case TypeCode.UInt16:
+                case TypeCode.UInt32:
+                case TypeCode.UInt64:
+                case TypeCode.Int16:
+                case TypeCode.Int32:
+                case TypeCode.Int64:
+                    return "int";
 
-				//case TypeCode.Object:
-				//	return "Any"; // must have "from typing import Any"
+                case TypeCode.Single:
+                case TypeCode.Decimal:
+                case TypeCode.Double:
+                    return "float";
 
-				default:
-					return type.Name;
+                case TypeCode.Boolean:
+                    return "bool";
 
-			}
-		}
-		#endregion
-	}
+                case TypeCode.Char:
+                case TypeCode.String:
+                    return "str";
+
+                case TypeCode.DateTime:
+                    return "datetime"; // must have "from datetime import datetime"
+
+                //case TypeCode.Object:
+                //	return "Any"; // must have "from typing import Any"
+
+                default:
+                    return type.Name;
+
+            }
+        }
+        #endregion
+    }
 }
