ec99ef09cf22803f9816f74682b2f03149e96340
MrMontana1889 <kris@culin.org>
2021-10-08 19:04:45 -0400

    Pre for handling an xml file that contains the settings to process - assemblies, classes, etc.

---
+++ Development/AssemblyCrawler.Xml/Config/ConfigClass.cs
@@ -0,0 +1,23 @@
+﻿// ConfigClass.cs
+// Copyright (c) 2021 Kristopher L. Culin See LICENSE for details
+
+using System;
+using System.Xml.Serialization;
+
+namespace AssemblyCrawler.Support.Config
+{
+	[XmlType("Class")]
+	public class ConfigClass
+	{
+		#region Constructor
+		public ConfigClass()
+		{
+
+		}
+		#endregion
+
+		#region Public Properties
+		public string Name { get; set; }
+		#endregion
+	}
+}
